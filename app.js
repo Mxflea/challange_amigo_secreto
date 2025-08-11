61ffb99f567994b85597c359ee19300bcab647b4
Maximiliano <siliano2000@gmail.com>
2025-08-11 22:14:05 +0100

    implement function to add friends

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = []
+
+function adicionarAmigo(){
+    let amigo = document.querySelector('input').value;
+
+    if (amigo == ''){
+        alert('Por favor, insira um nome.');
+    }else{
+        amigos.push(amigo);
+    }
+    limparCampo();
+    console.log('funciona');
+    console.log(amigos.length);
+}
+
+function limparCampo(){
+    amigo = document.querySelector('input');
+    amigo.value = '';
+}
