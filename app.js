6270f7e38234667d3bedd3bb2fd246b19c98e845
Maximiliano <siliano2000@gmail.com>
2025-08-16 23:38:52 +0100

    function update list html

--- app.js
+++ app.js
@@ -12,9 +12,22 @@ function adicionarAmigo(){
     limparCampo();
     console.log('funciona');
     console.log(amigos.length);
+    atualizaLista()
 }
 
 function limparCampo(){
     amigo = document.querySelector('input');
     amigo.value = '';
 }
+
+function atualizaLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++){
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
